a09a1ff0b8dc3c0a2a3d56be15abd8ff5e193307
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2024-11-18 11:07:53 -0500

    fix: funcion de tiempo de pedido procesado

--- src/components/KitchenOrders.tsx
+++ src/components/KitchenOrders.tsx
@@ -1,20 +1,48 @@
 import { useEffect, useState } from "react";
 import { Order } from "./OrdersView";
 import { getOrders, updateOrderStatus } from "../Services/api";
+import Layout from "./Layout";
 
 //gestionar pedidos por el jefe de cocina
 const KitchenOrders: React.FC = () => {
   const [orders, setOrders] = useState<Array<Order>>([]);
+  const [deliveryTimes, setDeliveryTimes] = useState<{ [key: number]: string }>(
+    {}
+  );
+  const date = new Date();
+  const dateFormatted = `${date.getFullYear()}-${date
+    .getDate()
+    .toString()
+    .padStart(2, "0")}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")} ${date.getHours().toString().padStart(2, "0")}:${date
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
+
   useEffect(() => {
     const loadOrders = async () => {
       const ordersData = await getOrders();
-      setOrders(ordersData);
+
+      const sortedOrders = ordersData.sort(
+        (a, b) =>
+          new Date(b.dataEntry).getTime() - new Date(a.dataEntry).getTime()
+      );
+      console.log(sortedOrders);
+      setOrders(sortedOrders);
+
+      sortedOrders.forEach((order) => {
+        if (order.status === "delivered" && order.dateProcessed) {
+          const time = calculateOrders(order.dataEntry, order.dateProcessed);
+          setDeliveryTimes((prev) => ({ ...prev, [order.id]: time }));
+        }
+      });
     };
     loadOrders();
   }, []);
   const updateStatus = async (orderId: number, newStatus: string) => {
-    const dateProcessed = new Date().toISOString();
-    console.log("dia procesado:", dateProcessed);
+    const dateProcessed = dateFormatted;
+
     await updateOrderStatus(orderId, newStatus, dateProcessed);
     setOrders(
       orders.map((order) =>
@@ -23,9 +51,35 @@ const KitchenOrders: React.FC = () => {
           : order
       )
     );
+    if (newStatus === "delivered") {
+      const order = orders.find((o) => o.id === orderId);
+      if (order) {
+        const time = calculateOrders(order.dataEntry, dateProcessed);
+
+        setDeliveryTimes((prev) => ({ ...prev, [orderId]: time }));
+      }
+    }
   };
+  //calaculate time of order status update from pending to delivered order
+  const calculateOrders = (
+    dataEntry: string,
+    dateProcessed: string
+  ): string => {
+    const entryTime = new Date(dataEntry).getTime();
+    const processedTime = new Date(dateProcessed).getTime();
+
+    // Diferencia en milisegundos
+    const timeDifference = processedTime - entryTime;
+
+    // Convertimos la diferencia a minutos
+    const minutes = Math.floor(timeDifference / (1000 * 60));
+    console.log("tiempo: " + minutes);
+    return `tiempo de preparación ${minutes} minutos`;
+  };
+
   return (
-    <section className="kitchenPanel">
+    <Layout>
+    <section className="menuContainer">
       <h1>Panel de cocina</h1>
       <ul>
         {orders.map((order) => (
@@ -33,6 +87,10 @@ const KitchenOrders: React.FC = () => {
             <h2>Pedido #{order.id}</h2>
             <p>Cliente: {order.client}</p>
             <p>Estado actual: {order.status}</p>
+            <p>Fecha: {order.dataEntry}</p>
+            {order.status === "delivered" && (
+              <p>Pedido entregado: {deliveryTimes[order.id]}</p>
+            )}
             <div className="status-buttons">
               <button
                 onClick={() => updateStatus(order.id, "pending")}
@@ -44,7 +102,7 @@ const KitchenOrders: React.FC = () => {
                 onClick={() => updateStatus(order.id, "delivering")}
                 disabled={order.status === "delivering"}
               >
-                En entrega
+                Listo para entregar
               </button>
               <button
                 onClick={() => updateStatus(order.id, "delivered")}
@@ -63,6 +121,7 @@ const KitchenOrders: React.FC = () => {
         ))}
       </ul>
     </section>
+    </Layout>
   );
 };
 export default KitchenOrders;
